02568908f6abe20540713f01fca8dfc6c21dfeea
stargeaze <yasminfsalma@gmail.com>
2024-05-22 17:18:39 +0700

    feat: Shop

--- src/main/java/org/awaludin/udinmaunikah/Application.java
+++ src/main/java/org/awaludin/udinmaunikah/Application.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Shop.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Awali dengan udin");
         stage.setScene(scene);
