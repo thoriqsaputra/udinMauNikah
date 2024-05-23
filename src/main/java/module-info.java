76574eaba1d4e1392047e0ac320634504ce42c5b
BiZaRrE96 <91012945+BiZaRrE96@users.noreply.github.com>
2024-05-23 21:21:45 +0700

    Fix json structure to actually use ID

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module org.awaludin.udinmaunikah {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires java.desktop;
+    requires json.simple;
 
     opens org.awaludin.udinmaunikah to javafx.fxml;
     exports org.awaludin.udinmaunikah;
