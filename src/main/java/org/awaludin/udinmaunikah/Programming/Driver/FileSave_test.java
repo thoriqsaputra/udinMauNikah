0d5d1386102a46f3e35c84c0174f779b5942e3ad
BiZaRrE96 <91012945+BiZaRrE96@users.noreply.github.com>
2024-05-24 21:38:36 +0700

    add getDeck because reflect is fussy with list

--- src/main/java/org/awaludin/udinmaunikah/Programming/Driver/FileSave_test.java
+++ src/main/java/org/awaludin/udinmaunikah/Programming/Driver/FileSave_test.java
@@ -1,12 +1,16 @@
 package org.awaludin.udinmaunikah.Programming.Driver;
 
 import org.awaludin.udinmaunikah.Programming.GameManager;
+import org.awaludin.udinmaunikah.Programming.GameObjectFactory;
 import org.awaludin.udinmaunikah.Programming.Loader;
 import org.awaludin.udinmaunikah.Programming.TXTLoader;
 
 public class FileSave_test {
     public static void main(String[] args) {
-        GameManager.initGameManager();;
+        GameManager.initGameManager();
+        GameObjectFactory.Load();
+        GameManager.SetUpUtils.useDeck(0,"default");
+        GameManager.SetUpUtils.useDeck(1,"default");
 
         Loader save = new TXTLoader();
 
